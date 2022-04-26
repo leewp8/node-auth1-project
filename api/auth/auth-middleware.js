0c7c7343c6897805997928f0b9ac70775fd30de0
William Lee <leewp8@gmail.com>
2022-04-26 00:25:20 -0400

    finished

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -8,7 +8,11 @@ const User = require('../users/users-model')
   }
 */
 function restricted(req, res, next) {
-  next()
+  if (req.session.user) {
+    next()
+  } else {
+    next({ status: 401, message: 'You shall not pass!'})
+  }
 }
 
 /*
@@ -40,8 +44,13 @@ async function checkUsernameFree(req, res, next) {
 async function checkUsernameExists(req, res, next) {
   try {
     const users = await User.findBy({ username: req.body.username})
-    if (users.length) next()
-    else next({ message: "Invalid credentials", status: 401})
+    if (users.length) {
+      req.user = users[0]
+      next()
+    }
+    else {
+      next({ message: "Invalid credentials", status: 401})
+    }
   } catch (err) {
     next(err)
   }
@@ -56,7 +65,11 @@ async function checkUsernameExists(req, res, next) {
   }
 */
 function checkPasswordLength(req, res, next) {
-  next()
+  if (!req.body.password || req.body.password.length < 3) {
+    next({ message: 'Password must be longer than 3 chars', status: 422})
+  } else {
+    next()
+  }
 }
 
 // Don't forget to add these to the `exports` object so they can be required in other modules
